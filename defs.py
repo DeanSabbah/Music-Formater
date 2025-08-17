1b09229d7722e86f9849d4abb779bb218d76c92a
Dean Sabbah <deanreynolds10@yahoo.ca>
2025-08-17 14:44:16 -0400

    working globals

--- defs.py
+++ defs.py
@@ -3,17 +3,13 @@ import logging
 logging.basicConfig(filename="log.txt", format='%(levelname)-8s:: %(filename)-8s @ %(asctime)s: %(message)s')
 
 
-# Quit flags
-global cancel_request
-global confiriming_quit
-# Basic global data
-global basepath
-global json
-#
-global logger
-
 def init():
+    global cancel_request  # noqa
     cancel_request = False
+    global confiriming_quit  # noqa
     confiriming_quit = False
+    global basepath  # noqa
+    global json  # noqa
     json = False
+    global logger  # noqa
     logger = logging.getLogger(__name__)
