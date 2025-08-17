c54dd1a5695724804edc8e5075a32b9fc6a746d9
Dean Sabbah <deanreynolds10@yahoo.ca>
2025-08-17 14:30:10 -0400

    Better globals?

--- defs.py
+++ defs.py
@@ -1,19 +1,19 @@
 import logging
 
 logging.basicConfig(filename="log.txt", format='%(levelname)-8s:: %(filename)-8s @ %(asctime)s: %(message)s')
-logger = logging.getLogger(__name__)
 
 
-def init(log_name:str=""):
-    # Quit flags
-    global cancel_request
+# Quit flags
+global cancel_request
+global confiriming_quit
+# Basic global data
+global basepath
+global json
+#
+global logger
+
+def init():
     cancel_request = False
-    global confiriming_quit
     confiriming_quit = False
-    # Basic global data
-    global basepath
-    global json
     json = False
-    #
-    global logger
     logger = logging.getLogger(__name__)
